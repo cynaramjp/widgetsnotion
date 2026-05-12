030b7f54659311bf4e7a3209a41a78cb0e5937cc
cynaramjp <cynaramjp@gmail.com>
2026-05-12 08:53:05 -0300

    Create sw.js

---
+++ planner/sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'agenda-v1';
+const ASSETS = [
+  './agenda.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/tone/14.8.49/Tone.js'
+];
+
+// Instalando o Service Worker e guardando arquivos no cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Respondendo requisições com cache quando offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
